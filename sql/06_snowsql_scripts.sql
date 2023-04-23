1d95100bb461f048ebb07593d9d8f9483edefb73
Brian Blaze <brianblaze14@outlook.com>
2023-04-23 20:27:19 +0100

    addition of the snowsql scripts

---
+++ sql/06_snowsql_scripts.sql
@@ -0,0 +1,30 @@
+-- Run the scripts in the SOWSQL command line
+use Reviews;
+select * from businesses limit 5;
+
+!source snowsql\CREATE TABLE SNOWSQL.txt
+!define cityName='Las Vegas'
+!set variable_substitution=true
+select * from top_businesses where city='&cityName';
+!set variable_substitution=false
+select * from top_businesses where city='&cityName';
+!define partialName='Las '
+!set variable_substitution=true
+select * from top_businesses where city='&{partialName}Vegas';
+
+!variables
+!spool snowsql\results.txt
+select * from businesses limit 5;
+
+-- Exports the file to the details
+!spool off
+!set output_format=csv
+!set header=false
+!set timing=false
+!spool snowsql\results.csv
+select * from businesses limit 5;
+!spool off
+!queries session
+-- Query id to be passed 01abd349-0000-5e7b-0000-000109f6216d
+!result ****************************
+!quit
